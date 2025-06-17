23f7199577e94f972267e666eb956672d85ead2c
Sahil <sahilsinger12347890@gmail.com>
2025-06-17 22:45:35 +0530

    Fix context usage, conditional rendering, and category filter mapping

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
 
 const App = () => {
-  return <div className="text-purple-800 bg-amber-400 rounded-lg ">sahil</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
